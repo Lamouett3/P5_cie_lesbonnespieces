addd233c5cfc227a5511b98e54add35d9a3cf7fb
Lamouett3 <d.aitmessaoud@gmail.com>
2022-08-29 21:23:53 +0200

    correction

--- pieces.js
+++ pieces.js
@@ -1,27 +1,32 @@
-// Récupération des pièces depuis le fichier JSON //
-    const reponse = await fetch("pieces-autos.json");
-    const pieces = await reponse.json();
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch("pieces-autos.json");
+const pieces = await reponse.json();
 
-// Création des éléments ett remplissage du texte ou de la source de l'image //
-    const ampoule = pieces [0];
+// Création des éléments et remplissage du texte ou de la source de l'image
+const ampoule = pieces[0];
 
-    const imageElement = document.createElement("img");
-    imageElement.src = ampoule.image;
+const imageElement = document.createElement("img");
+imageElement.src = ampoule.image;
 
-    const nomElement = document.createElement("h2");
-    nomElement.innertext = ampoule.nom;
+const nomElement = document.createElement("h2");
+nomElement.innerText = ampoule.nom;
 
-    const prixElement = document.createElement("p");
-    prixElement.innertext = "Prix : " + ampoule.prix + "€";
+const prixElement = document.createElement("p");
+prixElement.innerText = "Prix : " + ampoule.prix + " €"
+	+ " ("
+	+ (ampoule.prix < 35 ? "€" : "€€€")
+	+ ")";
 
-    const categorieElement = document.createElement("p");
-    categorieElement.innerText = ampoule.categorie;
+const categorieElement = document.createElement("p");
+categorieElement.innerText = ampoule.categorie ?? "(aucune catégorie)";
 
-    // Récupérartion de l'élément du DOM qui acceuillera les fiches //
-    const sectionFiches = document.querySelector(".fiches");
 
-    // Ajout des éléments créés dans le DOM //
-    sectionFiches.appendChild(imageElement);
-    sectionFiches.appendChild(nomElement);
-    sectionFiches.appendChild(prixElement);
-    sectionFiches.appendChild(categorieElement);
+// Récupération de l'élément du DOM qui accueillera les fiches
+const sectionFiches = document.querySelector(".fiches");
+
+// Ajout des éléments créés dans le DOM
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
+sectionFiches.appendChild(categorieElement);
+
